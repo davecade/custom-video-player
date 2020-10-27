ccc82035c9166d6428fe0eefcf5e19f30263bf82
Dave <dave.cadelina@outlook.com>
2020-10-28 01:27:46 +1100

    complete basic style, and auto play functionality only

---
+++ app.js
@@ -0,0 +1,26 @@
+const videoEl = document.querySelector('.screen')
+
+videoEl.play();
+
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (videoEl.requestFullscreen) {
+      videoEl.requestFullscreen();
+    } else if (videoEl.webkitRequestFullscreen) { /* Safari */
+      videoEl.webkitRequestFullscreen();
+    } else if (videoEl.msRequestFullscreen) { /* IE11 */
+      videoEl.msRequestFullscreen();
+    }
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+}
